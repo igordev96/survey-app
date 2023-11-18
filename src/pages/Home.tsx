737f528a3eac53c89f9d75f00ef8ecefa1b99b00
igordev96 <igordev96@gmail.com>
2023-11-18 20:56:48 +0000

    feature/SURVEYAPP-002: Create a loading spinner and a hook to check if the network is available

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import { Loading } from '../components/Loading';
+import { useOnline } from '../hooks/useOnline';
+
 export function Home() {
+  const { isOnline } = useOnline();
+
+  console.log(isOnline);
+
   return (
     <main className='min-h-screen bg-slate-800 text-white'>
       <h1>Home</h1>
       <h2>Hello, World!</h2>
+      <Loading />
     </main>
   );
 }
